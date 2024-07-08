d79ce97cb6f35c688421c081137a60c0c2d47aae
Andrei Novikau <andrei.novikau92@gmail.com>
2024-07-08 06:21:48 -0500

    add: shoppingCartPage.js

--- page_objects/shopingCart.js
+++ page_objects/shopingCart.js
@@ -1,10 +1,11 @@
 
 
-class ShopingCart {
+class ShoppingCart {
     constructor(page) {
         this.page = page;
         this.getShopingCartHeader = page.locator('h1.page-title');
+        this.getItemInShoppingCart = page.locator('.cart.item');
     }
 }
 
-export default ShopingCart;
+export default ShoppingCart;
